3193e227dc1929df11b9f421bf44316176f447a3
jisung hong <rhfktj@gmail.com>
2019-09-15 22:06:58 +0900

    Bring codes and show them with stdout

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,73 @@
 import * as rm from 'typed-rest-client/RestClient'
 import * as hm from 'typed-rest-client/HttpClient'
 import * as cheerio from 'cheerio'
+import Status from './type'
 
 let restc: rm.RestClient = new rm.RestClient('rest-samples', 'http://codeforces.com')
 let httpc: hm.HttpClient = new hm.HttpClient('codeforce-http')
 
-async function runContestList() {
-  let res: rm .IRestResponse<any> = await restc.get<any>('/api/contest.list?gym=true')
-  console.log(res.statusCode)
-  console.log(res.result)
+async function getContestList(): Promise<number[]> {
+  let res: rm .IRestResponse<any> = await restc.get<any>('/api/contest.list?gym=false')
+  
+  if (res.statusCode.toString()[0] !== '2') {
+    return []
+  }
+  
+  return res.result.result.filter(data => data.phase === 'FINISHED').map(data => data.id)
 }
 
-async function runContestStatus() {
-  let res: rm .IRestResponse<any> = await restc.get<any>('/api/contest.status?contestId=1209&from=1&count=10')
-  console.log(res.statusCode)
-  console.log(JSON.stringify(res.result, null, 2))
+async function getContestStatus(contestId, from, count): Promise<Status[]> {
+  let res: rm .IRestResponse<any> = await restc.get<any>(`/api/contest.status?contestId=${contestId}&from=${from}&count=${count}`)
+  
+  if (res.statusCode.toString()[0] !== '2') {
+    return []
+  }
+  
+  return res.result.result
 }
 
-async function getSourceCode() {
-  let res: hm.HttpClientResponse = await httpc.get('http://codeforces.com/contest/1209/submission/60532598')
-  console.log(res.message.statusCode)
+async function getSourceCode(contestId, submissionId): Promise<string> {
+  let res: hm.HttpClientResponse = await httpc.get(`http://codeforces.com/contest/${contestId}/submission/${submissionId}`)
+  if (res.message.statusCode.toString()[0] !== '2') {
+    return 'fail'
+  }
+  
   let body: string = await res.readBody()
   
   const parsed = cheerio.load(body)
   const sourceCode = parsed('#program-source-text')[0].children[0].data
-  console.log(sourceCode)
+  return sourceCode
 }
 
-getSourceCode()
+async function getCodesFromContest() {
+  const contestlist = await getContestList()
+  console.log(`contest list count: ${contestlist.length}`)
+
+  for await (const contestId of contestlist) {
+    let from = 1;
+    const count = 100;
+    while (true) {
+      const contestStatus = await getContestStatus(contestId, from, count)
+      console.log(`read contest ${contestId} from ${from} to ${from + contestStatus.length - 1}`)
+      from += count
+      if (contestStatus.length <= 0) {
+        break;
+      }
+
+      for await (const status of contestStatus) {
+        const sourceCode = await getSourceCode(status.contestId, status.id)
+        console.log(`contestId: ${status.contestId}`)
+        console.log(`submissionId: ${status.id}`)
+        console.log(`user handle: ${JSON.stringify(status.author.members, null, 2)}`)
+        console.log(`source code:\n${sourceCode}`)
+      }
+      return
+    }
+  }
+}
+
+try {
+  getCodesFromContest()
+} catch (e) {
+  console.log(e)
+}
---
+++ src/type.ts
@@ -0,0 +1,40 @@
+// http://codeforces.com/apiHelp/objects
+interface Problem {
+  contestId: number,
+  problemsetName: string,
+  index: string,
+  name: string,
+  type: string,
+  points: number,
+  rating: number,
+  tag: string[]
+}
+
+interface Member {
+  handle: string // user handle
+}
+
+interface Party {
+  contestId: number,
+  members: Member[],
+  participantType: string,
+  teamId?: number,
+  ghost: boolean,
+  room?: number,
+  startTimeSeconds?: number
+}
+
+export default interface Status {
+  id: number,
+  contestId: number,
+  creationTimeSeconds: number,
+  relativeTimeSeconds: number,
+  problem: Problem,
+  author: Party,
+  programmingLanguage: string,
+  verdict: string,
+  testset: string,
+  passedTestCount: number,
+  timeConsumedMillis: number,
+  memoryConsumedBytes: number
+}
